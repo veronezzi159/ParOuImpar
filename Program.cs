af512b4d6aa2f4641c38a6c3f692303000d6ae8d
L.Veronezzi <L.Veronezzi@DESKTOP-IQ2PR1Q>
2024-04-19 00:38:45 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿decimal number;
+
+Console.WriteLine("Digite um número");
+number = decimal.Parse(Console.ReadLine()); 
+
+if (number % 2 == 0) 
+{
+    Console.WriteLine(number + " É par");
+
+} else
+{
+    Console.WriteLine(number + " É ímpar");
+}
